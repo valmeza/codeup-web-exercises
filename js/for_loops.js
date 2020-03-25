59edb5e2b15305d0bc64dae0a9b76d417fbbbc5e
Valeria Meza <valeriameza173@gmail.com>
2020-03-24 23:41:26 -0500

    completed another loop exercise.

--- js/for_loops.js
+++ js/for_loops.js
@@ -5,4 +5,28 @@ function showMultiplicationTable (number) {
         console.log(number + ' X ' + i  + " = "+ (number * i));
     }
 }
-showMultiplicationTable(7);
+showMultiplicationTable(3);
+
+
+Math.floor(Math.random() * 200) + 20;
+
+for (var i = 1, random = Math.floor(Math.random() * 200) + 20; i <= 10; i++) {
+    if (random % 2 === 0 ) {
+        console.log(random + " is even");
+        random++;
+    } else {
+        console.log(random + " number is odd");
+        random++;
+    }
+}
+
+
+for (i = 1; i <= 9; i++) {
+    console.log(i);
+}
+
+for (i = 100; i >= 5 ; i--) {
+    if (i % 5 === 0) {
+        console.log(i);
+    }
+}
