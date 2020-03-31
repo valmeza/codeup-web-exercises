9c288c36ed8a12f638510b5156f3471db5c0fe76
Valeria Meza <valeriameza173@gmail.com>
2020-03-30 21:32:59 -0500

    minor changes

--- js/objects.js
+++ js/objects.js
@@ -68,7 +68,6 @@
         }
     });
 
-
     /** TODO:
      * Create an array of objects that represent books and store it in a
      * variable named `books`. Each object should have a title and an author
