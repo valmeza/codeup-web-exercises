eba99f37ab1ffbbfda1cc52ee56493831f4b2501
Valeria Meza <valeriameza173@gmail.com>
2020-04-02 14:27:26 -0500

    completed the mapbox lec

--- js/mapbox-api-lec.js
+++ js/mapbox-api-lec.js
@@ -1,10 +1,9 @@
 /**********************************************
- * 			SETTING UP KEYS.JS
+ *            SETTING UP KEYS.JS
  *********************************************/
 // TODO TOGETHER: Open .gitignore and add keys.js. Add keys.js file and import to mapbox html file. Your api keys are stored in keys.js and are added to the .gitignore so they are protected
-console.log('Token:' + mapboxToken);
 /**********************************************
- * 			CUSTOMIZING THE MAP
+ *            CUSTOMIZING THE MAP
  *********************************************/
 // Predefined map styles --> https://docs.mapbox.com/mapbox-gl-js/api/#map
 
@@ -13,26 +12,27 @@ console.log('Token:' + mapboxToken);
 // Zoom levels range from 0 up to 24, with 0 being a global view and 24 being the most detailed at street level (the max zoom level depends on the location).
 
 //TODO TOGETHER: Set map to Dallas area using the coordinates [-96.8057, 32.7787]
+
 mapboxgl.accessToken = mapboxToken;
-var map = new mapboxgl.Map({
-    container: 'map',
-    // style: 'mapbox://styles/mapbox/streets-v9',
-    // style: "mapbox://styles/mapbox/dark-v10",
-    // style: "mapbox://styles/mapbox/navigation-preview-night-v4",
-    style: "mapbox://styles/mapbox/navigation-guidance-night-v4",
-    center: [-97.508469, 35.481918],
-    zoom: 11,
-    clickTolerance: 3,
-    keyboard: true,
-    doubleClickZoom: true
-});
+// var map = new mapboxgl.Map({
+//     container: 'map',
+//     // style: 'mapbox://styles/mapbox/streets-v9',
+//     // style: "mapbox://styles/mapbox/dark-v10",
+//     // style: "mapbox://styles/mapbox/navigation-preview-night-v4",
+//     style: "mapbox://styles/mapbox/navigation-guidance-night-v4",
+//     center: [-97.508469, 35.481918],
+//     zoom: 11,
+//     clickTolerance: 3,
+//     keyboard: true,
+//     doubleClickZoom: true
+// });
 
 
 //TODO: Experiment with different map styles, zoom levels, and centers. You will need to reference the mapbox docs. (~15 minutes)
 
 
 /**********************************************
- * 					MARKERS
+ *                    MARKERS
  *********************************************/
 // Marker Docs --> https://docs.mapbox.com/mapbox-gl-js/api/#marker
 // Markers are specific locations on a map
@@ -41,25 +41,26 @@ var map = new mapboxgl.Map({
 
 // TODO TOGETHER: Add a marker to the map using the following coordinates [-96.8084, 32.7799]. This marker will mark the Sixth Floor Muesume on our map.
 // TODO TOGETHER: Change the color of the marker
-var markerOptions = {
-    color: "purple",
-    draggable: true
-};
 
-var marker = new mapboxgl.Marker(markerOptions)
-    .setLngLat([-97.508469, 35.481918])
-    .addTo(map);
-
-var markerMuseum = new mapboxgl.Marker(markerOptions)
-    .setLngLat([-96.8084, 32.7799])
-    .addTo(map);
+// var markerOptions = {
+//     color: "purple",
+//     draggable: true,
+// };
+//
+// var marker = new mapboxgl.Marker(markerOptions)
+//     .setLngLat([-97.508469, 35.481918])
+//     .addTo(map);
+//
+// var markerMuseum = new mapboxgl.Marker(markerOptions)
+//     .setLngLat([-96.8084, 32.7799])
+//     .addTo(map);
 
 // TODO: Experiment with the color, and setting the LngLat
 // TODO: Update the marker object to make the marker draggable. *Hint: reference the docs!
 
 
 /**********************************************
- * 					POPUPS
+ *                    POPUPS
  *********************************************/
 // Popups are the info boxes that appear on a map and may describe a given location.
 // Popup docs --> https://docs.mapbox.com/mapbox-gl-js/api/#popup
@@ -67,46 +68,62 @@ var markerMuseum = new mapboxgl.Marker(markerOptions)
 
 // TODO TOGETHER: Add a popup to the map over codeup. Set the html as a paragraph that says "Codeup Rocks!"
 
-var popup = new mapboxgl.Popup()
-    .setHTML("<h1>Codeup Rocks</h1>")
-    .addTo(map);
-
-marker.setPopup(popup);
+// var popup = new mapboxgl.Popup()
+//     .setHTML("<h1>Codeup Rocks</h1>")
+//     .setMaxWidth('120px')
+//     .addTo(map);
+//
+// marker.setPopup(popup);
 
 // TODO TOGETHER: Comment out the popup we just added. Add a popup to the Sixth Floor Museum marker.
 
-var popupMuseum = new mapboxgl.Popup()
-    .setHTML("<h1>Sixth Floor Museum</h1>")
-    .addTo(map);
-
-markerMuseum.setPopup(popupMuseum);
+// var popupMuseum = new mapboxgl.Popup()
+//     // .setHTML("<h1>Sixth Floor Museum</h1>")
+//     // TODO: Try setting the text by using ".setText()" instead of ".setHTML()"
+//     .setText('Hello, People!')
+//     .setMaxWidth('100px')
+//     .addTo(map);
+//
+// markerMuseum.setPopup(popupMuseum);
 
 // TODO: Review the popup docs. What are some additional options we can pass to the popup?
-// TODO: Try setting the text by using ".setText()" instead of ".setHTML()"
-
 
 
 /**********************************************
- * 					Geocoder
+ *                    Geocoder
  *********************************************/
 // Geocoding Docs --> https://docs.mapbox.com/api/search/#geocoding
 
 
 // TODO TOGETHER: Using the Geocoder helper function, log the coordinates of Codeup and recenter the map to focus on Codeup. Comment out previous map code.
 
-
-
-
-
+geocode("701 Commerce St. Dallas, TX, 75202", mapboxToken).then(function (result) {
+    console.log(result);
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 16
+    });
 //TODO: Using the geocode method above, add a marker at Codeup to the map
 //TODO: Instead of setCenter try using map.jumpTo()
 //TODO: Instead of setCenter try using map.flyTo()
-
+    // map.jumpTo({center: result});
+    // map.flyTo({center: result});
+    map.setCenter(result);
+    var markerOptions = {
+        color: "purple",
+        draggable: true,
+    };
+    var marker = new mapboxgl.Marker(markerOptions)
+        .setLngLat(result)
+        .addTo(map);
+});
 
 
 // TODO TOGETHER: Reverse Geocoding: Using the reverse geocoding method, enter the coordinates {lng: -96.8084, lat: 32.7799} to get a physical address for the Sixth Floor Musume
-
-
+reverseGeocode({lng:-96.8084, lat: 32.7799}, mapboxToken).then(function(result) {
+    console.log(result)
+});
 
 // TODO: Reverse geocode coordinates of your choice using the reverse geocode method
 
