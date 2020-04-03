b5cb6373e071b42ea54776aec39d8e62483e4950
Valeria Meza <valeriameza173@gmail.com>
2020-04-03 08:55:35 -0500

    some modifications

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -1,11 +1,27 @@
 // Token access here
 mapboxgl.accessToken = mapboxToken;
 
-geocode("Western Ave, Oklahoma City, OK 73109", mapboxToken).then(function (result){
+geocode("3903 S Western Ave, Oklahoma City, OK 73109", mapboxToken).then(function (result){
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v9',
         zoom: 18
     });
-    map.flyTo({center: result});
+    map.setCenter(result);
+
+    var markerOptions = {
+        color: 'purple',
+        draggable: true
+    };
+    var marker = new mapboxgl.Marker(markerOptions)
+        .setLngLat(result)
+        .addTo(map);
+
+    map.on('click', function () {
+        var favRestaurant = new mapboxgl.Popup()
+            .setText("La Tropicana")
+            .addTo(map);
+
+        marker.setPopup(favRestaurant);
+    });
 });
