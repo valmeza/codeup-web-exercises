615012e5c763ff827279541c025b735bb1eba241
Valeria Meza <valeriameza173@gmail.com>
2020-04-22 16:14:57 -0500

    tried my best in this exercise.

--- js/promises.js
+++ js/promises.js
@@ -1,34 +1,45 @@
 "use strict";
-const url = 'https://api.github.com/users';
-console.log(url);
+// const url = 'https://api.github.com/users/valmeza/events/public';
+// console.log(url);
 
 // fetch(url, {headers: {'Authorization': `token ${PROMISE_KEY}`}}).then(response => {
 //     response.json().then(user => {
 //         user.forEach(userObj => {
-//             console.log(userObj.login)
+//             console.log(userObj)
 //         });
 //     });
 // });
 
-function status(response) {
-    if (response.status >= 200 && response.status < 300) {
-        return Promise.resolve(response)
-    } else {
-        return Promise.reject(new Error(response.statusText))
-    }
-}
-function json(response) {
-    return response.json()
-}
+// function status(response) {
+//     if (response.status >= 200 && response.status < 300) {
+//         return Promise.resolve(response)
+//     } else {
+//         return Promise.reject(new Error(response.statusText))
+//     }
+// }
+// function json(response) {
+//     return response.json()
+// }
+//
+// fetch(url, {headers: {'Authorization': `token ${PROMISE_KEY}`}})
+//     .then(status)
+//     .then(json)
+//     .then(function (data) {
+//         console.log('Request succeeded with JSON response', data);
+//         data.forEach(data => {
+//             console.log(data);
+//
+//         })
+//     }).catch(function (error) {
+//     console.log('Request failed', error);
+// });
 
-fetch(url)
-    .then(status)
-    .then(json)
-    .then(function (data) {
-        console.log('Request succeeded with JSON response', data);
-        data.forEach(data => {
-            console.log(data.login);
-        })
-    }).catch(function (error) {
-    console.log('Request failed', error);
-});
+function getLastCommit(username) {
+    let url = `https://api.github.com/users/${username}/events/public`;
+    console.log(url);
+    return fetch(url, {headers: {'Authorization': `token ${PROMISE_KEY}`}})
+        .then(response => response.json()
+            .then (data => data[0].created_at)
+        )
+}
+getLastCommit('valmeza').then(lastCommit => document.body.innerHTML = `${lastCommit}`);
