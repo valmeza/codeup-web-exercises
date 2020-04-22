aedbacb4ff0d72df84fa51fea4ef7531388eb553
Valeria Meza <valeriameza173@gmail.com>
2020-04-22 14:16:11 -0500

    playing with functions

--- js/promises.js
+++ js/promises.js
@@ -2,4 +2,33 @@
 const url = 'https://api.github.com/users';
 console.log(url);
 
-fetch(url, {headers: {'Authorization': `token ${PROMISE_KEY}`}});
+// fetch(url, {headers: {'Authorization': `token ${PROMISE_KEY}`}}).then(response => {
+//     response.json().then(user => {
+//         user.forEach(userObj => {
+//             console.log(userObj.login)
+//         });
+//     });
+// });
+
+function status(response) {
+    if (response.status >= 200 && response.status < 300) {
+        return Promise.resolve(response)
+    } else {
+        return Promise.reject(new Error(response.statusText))
+    }
+}
+function json(response) {
+    return response.json()
+}
+
+fetch(url)
+    .then(status)
+    .then(json)
+    .then(function (data) {
+        console.log('Request succeeded with JSON response', data);
+        data.forEach(data => {
+            console.log(data.login);
+        })
+    }).catch(function (error) {
+    console.log('Request failed', error);
+});
